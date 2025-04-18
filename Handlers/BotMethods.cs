9ceadd397a3b981d69b977f41d3c8bf8de30f0b2
MRtim <timatosalikaze@gmail.com>
2025-04-18 21:18:34 +0500

    не рабботают кнопки не, переводит ничего

--- Handlers/BotMethods.cs
+++ Handlers/BotMethods.cs
@@ -1,72 +1,116 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.VisualBasic;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
-using Telegram_Bot;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using GTranslatorAPI;
+using static System.Net.Mime.MediaTypeNames;
+using System.Web;
 
 namespace My_telegram_bot.Handlers
 {
     internal class BotMethods
     {
-        private static string BotToken = "7683673191:AAHoIE-NDTggGT4AhzuCdzZCoJtC5JiqasY";
-        void OnMessage()
+        public async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken ct)     /// Сообщение пользователя и ответ
         {
-            async Task HandleUpdateAsync(Message msg, UpdateType type)                                     /// Сообщение пользователя и ответ
+            try
             {
-                using var cts = new CancellationTokenSource();
-                var bot = new TelegramBotClient(BotToken, cancellationToken: cts.Token);
-                var me = await bot.GetMe();
-                Console.WriteLine($"Бот запущен. ID: {me.Id}, Имя: {me.FirstName}");
-
-                Console.WriteLine("Enter - остановаить бота");
-                Console.ReadLine();
-                cts.Cancel();
-                Console.WriteLine($"пользователь {msg.From}. Написал: {msg.Text}. В чате(ID) :{msg.Id}");
-                string UserMessage = msg.Text!;
-                switch (UserMessage)
+                if (update.Message is { } message)
                 {
-                    case "/start":
-                        await bot.SendMessage(msg.Chat, $"Привет, {msg.Id}! Этот бот позволяет переводить сообщения",
-                        replyMarkup: new InlineKeyboardButton[] { "с англ на рус", "с рус на англ" });
-                        break;
-                    case "/":
-                        break;
-                    case "Привет" or "привет":
-                        await bot.SendMessage(msg.Id, "Пока:)");
-                        break;
+                    await GetUpdate(bot, message, update);
+                }
+                else if (update.CallbackQuery is { } callbackQuery)
+                {
+                    ////await HandleCallbackQuery(bot, callbackQuery);
                 }
             }
-        }
-        void OnUpdate()
-        {
-            async Task OnUpdate(Update update)                                                      /// какую кнопку нажал пользователь
+            catch (Exception ex)
             {
-                using var cts = new CancellationTokenSource();
-                var bot = new TelegramBotClient(BotToken, cancellationToken: cts.Token);
-                if (update is { CallbackQuery: { } query })
+                Console.WriteLine($"Ошибка обработки обновления: {ex.Message}");
+            }
+            
+        }
+        public async Task GetUpdate(ITelegramBotClient bot, Message msg, Update update)
+        {
+            var message = msg.Text!;
+            var chatId = msg.Chat.Id;
+            Console.WriteLine($"{msg.From} пишет: {message}. ID чата : {chatId}");
+            switch (message)
+            {
+                case "/start":
+                    await bot.SendMessage(chatId, $"Привет, {msg.From!.FirstName}! Этот бот позволяет переводить сообщения.");
+                    await Task.Delay(100);
+                    break;
+                case "/author":
+                    await bot.SendMessage(chatId, "@Tlmyr");
+                    await bot.SendContact(chatId, "+79528637399", "Тимур");
+                    await Task.Delay(100);
+                    break;
+                case "/translate":
+                    await Translate(bot, chatId, msg, update);
+                    break;
+                case "/help":
+                    await bot.SendMessage(chatId, "/tranlate - перевод сообщений");
+                    await bot.SendMessage(chatId, "/author - автор");
+                    break;
+                default:
+                    await bot.SendMessage(chatId, "я не понял");
+                    break;
+            }
+        }
+
+        public async Task Translate(ITelegramBotClient bot, ChatId chatId, Message msg, Update update)      //Перевод
+        {
+            HttpClient client = new HttpClient();
+            string apiKey = "dict.1.1.20250418T142716Z.238261338557aea8.61b7dda74540dd85e88d1aee02b3e15668e082ac";
+            var keyboard = new InlineKeyboardMarkup(new[] { new[] { InlineKeyboardButton.WithCallbackData("Английский", "Английский"), InlineKeyboardButton.WithCallbackData("Китайский", "Китайский") } });
+            await bot.SendMessage(msg.Chat, "На каком языке хотите перевод?", replyMarkup: keyboard);
+            var updates = new List<Update>{new Update { CallbackQuery = new CallbackQuery { Data = "Английский" } }, new Update { CallbackQuery = new CallbackQuery { Data = "Китайский" } } };
+            foreach (var update1 in updates)
+            {
+                if (update.CallbackQuery is { } callbackQuery)
                 {
-                    await bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
-                    await bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
+                    string langCode = callbackQuery.Data;
+                    switch (langCode)
+                    {
+                        case " Английский":
+                            await bot.AnswerCallbackQuery(callbackQueryId: callbackQuery.Id, "Напишите слово и я его переведу");
+                            var wordEn = msg.Text;
+                            string Translated1 = $"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={apiKey}&lang=ru-en&text={wordEn}";
+                            string JSONen = await client.GetStringAsync(apiKey); 
+                            await bot.SendMessage(chatId, JSONen);
+                            break;
+                        case "Китайский":
+                            await bot.AnswerCallbackQuery("Напишите слово и я его переведу");
+                            var wordZh = msg.Text;
+                            string TranslatedZh = $"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={apiKey}&lang=ru-zh&text={wordZh}";
+                            string JSONzh = await client.GetStringAsync(apiKey);
+                            await bot.SendMessage(chatId, JSONzh);
+                            break;
+                        default:
+                            await bot.SendMessage(chatId, "я не понял");
+                            break;
+                    }
                 }
             }
+            
+            
+
         }
-        static void OnError()
+        public Task HandlePollingErrorAsync(ITelegramBotClient bot, Exception exception, CancellationToken ct)
         {
-            async Task OnError(Exception exception, HandleErrorSource source)                      /// Обработка ошибок
-            {
-                Console.WriteLine(exception);
-            }
+            Console.WriteLine($"Ошибка: {exception.Message}");
+            return Task.CompletedTask;
         }
-    }
-    
-
-        
-  
-
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -5,19 +5,37 @@ using Telegram.Bot.Types;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.ReplyMarkups;
+using Microsoft.VisualBasic;
+using System.Threading;
+using GTranslatorAPI;
 
 namespace Telegram_Bot
 {
-    class Program 
+    class Program
     {
-        
         static async Task Main(string[] args)
         {
-            BotMethods botMethods;
-            botMethods = new BotMethods();
-            botMethods.OnMessage();
-            botMethods.OnUpdate();
-            botMethods.OnError();
+            Console.WriteLine("Бот запущен.");
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var bot = new TelegramBotClient("7973585300:AAGXOuR9Hb7wRj6Nihqpa2PFygPflf_cuYg");
+            var Exception = new Exception();
+            var receiverOptions = new ReceiverOptions { AllowedUpdates = new[] { UpdateType.Message, UpdateType.CallbackQuery } };
+            var update = new Update();
+
+            var botMethods = new BotMethods();
+
+            var me = await bot.GetMe();
+            Console.WriteLine($"ID: {me.Id}, Имя: {me.FirstName}");
+
+            bot.StartReceiving(
+            updateHandler: botMethods.HandleUpdateAsync,
+            botMethods.HandlePollingErrorAsync,
+            receiverOptions,
+            cts.Token
+        );
+            Console.WriteLine("Enter - остановка");
+            Console.ReadLine();
+            cts.Cancel();
+            }
         }
     }
-}
